7eb98f5b4da0b747d3c8b6bb4114bc7150c55970
Jacob Harrington <jharrington4@gmail.com>
2019-10-29 10:58:10 -0700

    keyword filter working

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ import { renderAnimal } from './util/render-gallery.js';
 import htmlToDOM from './util/html-to-DOM.js';
 
 const listItem = document.querySelector('.horned-guys');
+const keywordFilter = document.querySelector('.keyword-select');
 
-images.forEach(image => {
-    const htmlContent = renderAnimal(image);
-    const dom = htmlToDOM(htmlContent);
-    listItem.appendChild(dom);
+const renderSelectedAnimal = (renderedAnimal) => {
+    while (listItem.lastElementChild){
+        listItem.lastElementChild.remove();
+    }
+
+    renderedAnimal.forEach(animal => {
+        const htmlContent = renderAnimal(animal);
+        const dom = htmlToDOM(htmlContent);
+        listItem.appendChild(dom);
+    });
+};
+
+keywordFilter.addEventListener('change', () => {
+    const keyword = keywordFilter.value;
+    let filteredHorns = null;
+
+    if (!keyword) {
+        filteredHorns = images;
+    }
+    else {
+        filteredHorns = images.filter(image => {
+            return image.keyword === keyword;
+        });
+    }
+
+    renderSelectedAnimal(filteredHorns);
 });
+
+renderSelectedAnimal(images);
